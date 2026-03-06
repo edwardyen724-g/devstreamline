6c3ca39b686177738168471748b01786ff4eecd7
edwardyen724-g <65644998+edwardyen724-g@users.noreply.github.com>
2026-03-06 03:45:33 -0800

    feat: add tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,21 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/pages/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         primary: '#3B82F6', // Custom primary color
-        secondary: '#1D4ED8', // Custom secondary color
-        accent: '#FBBF24', // Custom accent color
-      },
-      spacing: {
-        '128': '32rem', // Custom spacing
-        '144': '36rem',
+        secondary: '#FBBF24', // Custom secondary color
+        accent: '#4F46E5', // Custom accent color
       },
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans], // Custom font
+        sans: ['Inter', ...fontFamily.sans],
+        mono: ['Fira Code', ...fontFamily.mono],
       },
     },
   },
